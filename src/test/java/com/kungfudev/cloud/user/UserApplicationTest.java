0e2e003b970a203950c65cbd2d3f0f6679393255
Kevin Sewell <kevin.w.sewell@gmail.com>
2015-12-13 18:38:49 +0200

    Attempting to get selenium tests working with hub

--- src/test/java/com/kungfudev/cloud/user/UserApplicationTest.java
+++ src/test/java/com/kungfudev/cloud/user/UserApplicationTest.java
@@ -1,17 +1,17 @@
 package com.kungfudev.cloud.user;
 
-import com.kungfudev.cloud.common.tests.SeleniumTest;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.net.URL;
 
 
 /**
@@ -21,11 +21,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TestUserApplication.class)
-@WebIntegrationTest(value = "server.port=9000")
-@SeleniumTest(driver = ChromeDriver.class, baseUrl = "http://localhost:9000/resources/user.html")
+@WebIntegrationTest(value = "server.port=8080")
+//@SeleniumTest(driver = ChromeDriver.class, baseUrl = "http://gocd-build-agent:8080/resources/user.html")
 public class UserApplicationTest {
 
-    @Autowired
     private WebDriver driver;
 
     private UserPage userPage;
@@ -33,6 +32,10 @@ public class UserApplicationTest {
     @Before
     public void setUp() throws Exception {
         userPage = PageFactory.initElements(driver, UserPage.class);
+        URL remoteAddress = new URL("http://selenium.aronim.com");
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+        driver = new RemoteWebDriver(remoteAddress, desiredCapabilities);
+        driver.get("http://gocd-build-agent:8080/resources/user.html");
     }
 
     @Test
